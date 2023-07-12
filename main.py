e1bed7df096f88905224ae424109a6f641960b71
Leo Bueno <leobueno.dev@gmail.com>
2023-07-12 08:36:30 -0300

    ⚡️ Melhora as mensagens conforme status

--- main.py
+++ main.py
@@ -48,7 +48,15 @@ async def recWebHook(req: Request):
         issue_url = body["issue"]["html_url"]
         isue_label = await getLabels(body["issue"]["labels"])
 
-        message = f"New issue([{issue_number}]({issue_url})) on [{issue_repo}]({issue_repo_url}) \nstatus: {issue_action} \nby: [{issue_login}]({issue_login}).\n\nTitle: *{issue_title}* \n\nDescription: {issue_desc} \n\nLabels: {isue_label}"
+        if issue_action == "opened":
+            logging.warning("Issue Opened")
+            # message = f"*Issue* ([{issue_number}]({issue_url})) on [{issue_repo}]({issue_repo_url}) \n\nstatus: {issue_action} \nby: [{issue_login}]({issue_login}).\n\nTitle: *{issue_title}* \n\nDescription: {issue_desc} \n\nLabels: {isue_label}"
+
+        if issue_action == "assigned":
+            message = f"Issue ([{issue_number}]({issue_url})) on [{issue_repo}]({issue_repo_url}) *assigned* to: {issue_action} \n\nby: [{issue_login}]({issue_login}).\n\nTitle: *{issue_title}* \n\nLabels: {isue_label}"
+
+        if issue_action == "closed":
+            message = f"Issue ([{issue_number}]({issue_url})) on [{issue_repo}]({issue_repo_url}) *closed* by: [{issue_login}]({issue_login}).\n\nTitle: *{issue_title}* \n\n"
 
     elif event == "pull_request":  # check if event is a pull request
         # pprint.pprint(body)
